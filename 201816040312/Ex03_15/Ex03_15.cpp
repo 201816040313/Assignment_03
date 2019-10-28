b3ead1c48a480cf765f7fa9eaf7aa99b60ce6e56
201816040312 <56497125+201816040312@users.noreply.github.com>
2019-10-28 13:19:05 +0800

    Create Ex03_15.cpp

---
+++ 201816040312/Ex03_15/Ex03_15.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+#include"data.h"
+int main()
+{
+    Data debt(2019,10,27);// create a Data object; pass a year,month,day
+    debt.displayData();//ouput the data
+}
